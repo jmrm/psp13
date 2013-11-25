b4daa790ad9b736cbb729d7f3e6ad5d70bad896d
jmrm <prof.inf@gmail.com>
2013-11-25 10:16:06 +0100

    Pasamos ejercicios de hilos a Tema02

--- Tema01/src/Hilos_01/HiloSimple.java
+++
@@ -1,7 +0,0 @@
-package Hilos_01;
-
-public class HiloSimple extends Thread {
-	public void run() {
-		for (int i=0; i<10; i++) System.out.println("Desde dentro del hilo "+i);
-	}
-}
--- Tema01/src/Hilos_01/HiloSimple2.java
+++
@@ -1,7 +0,0 @@
-package Hilos_01;
-
-public class HiloSimple2 implements Runnable {
-	public void run() {
-		for (int i=0; i<10; i++) System.out.println("Desde dentro del hilo "+i);
-	}
-}
