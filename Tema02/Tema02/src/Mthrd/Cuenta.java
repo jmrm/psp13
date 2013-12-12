6f7b287612a5343e6d2c1d9eafe9de475f1c9d24
jmrm <prof.inf@gmail.com>
2013-12-12 14:55:35 +0100

    Añadida la solución del profesor a la segunda parte de PSP. Tema 02. Ejercicio 01

---
+++ Tema02/Tema02/src/Mthrd/Cuenta.java
@@ -0,0 +1,54 @@
+package Mthrd;
+/*
+ * Esta es la clase Cuenta pedida. El código de cuenta no
+ * es controlado (en lo que se refiere a los códigos "reales" de un banco)
+ * solamente en cuanto al tamaño, que si es más de 10 se trunca y si es menor
+ * se rellena con espacios en blanco
+ */
+public class Cuenta {
+	private String numCuenta;
+	double saldo;
+	public Cuenta (String c,double s) {
+		StringBuilder sb=new StringBuilder();
+		String relleno="          ";
+		sb.append(c);
+		// Si c tiene más de 10 caracteres truncamos 
+		if (sb.length()>10) sb.setLength(10);
+		else // Si c tiene menos de 10 caracteres, rellenamos con espacios
+			if(sb.length()<10) 
+				sb.append(relleno.substring(sb.length()));
+		saldo=s;numCuenta=sb.toString();
+	}
+	public Cuenta() {
+		this("0123456789",100);
+	}
+	public String getNumCuenta() {
+		return this.numCuenta;
+	}
+	public void setNumCuenta(String numCuenta) {
+		this.numCuenta = numCuenta;
+	}
+	public double getSaldo() {
+		return this.saldo;
+	}
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+	public void print() {
+		System.out.print("Cuenta num: "+numCuenta+" saldo: "+saldo);
+	}
+	public void println() {
+		print();System.out.println();
+	}
+	public double ingresar(double i) {
+		this.saldo+=i;
+		return this.saldo;
+	}
+	public double reintegrar (double r) throws Exception {
+		if (r<=this.saldo) {
+			this.saldo-=r;
+		} else
+			throw new Exception("Saldo insuficiente: reintegro "+r+" y solo hay "+this.saldo);
+		return saldo;
+	}
+}	
